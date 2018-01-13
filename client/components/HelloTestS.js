250491805bf3d0c89ba0e5a525e5a2b9da658b0a
AdamReszka <adam.reszka85@gmail.com>
2018-01-13 10:38:51 +0100

    add files for first mern project

---
+++ client/components/HelloTestS.js
@@ -0,0 +1,13 @@
+import React, { PropTypes } from 'react';
+
+let HelloTestS = (props, context) => {
+  return (
+    <h1>Arrow test</h1>
+  );
+}
+
+
+HelloTestS.propTypes = {
+};
+
+export default HelloTestS;
---
+++ components/HelloTestM/HelloTestM.js
@@ -0,0 +1,13 @@
+import React, { PropTypes } from 'react';
+
+let HelloTestM = (props, context) => {
+  return (
+    <h1>Arrow test</h1>
+  );
+}
+
+
+HelloTestM.propTypes = {
+};
+
+export default HelloTestM;
